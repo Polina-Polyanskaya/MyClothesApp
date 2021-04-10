590e89309729f129a5a756a44086700c7331a84b
Polina-Polyanskaya <papolyanskaya@edu.hse.ru>
2021-04-10 12:07:09 +0300

    New pages were added

--- app/src/main/java/com/example/clothesapp/MyCatalog.kt
+++ app/src/main/java/com/example/clothesapp/MyCatalog.kt
@@ -1,11 +1,19 @@
 package com.example.clothesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MyCatalog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_catalog)
+        getSupportActionBar()?.hide()
+    }
+
+    fun toNextActivitySearch(view: View) {
+        val intent = Intent(this, TypeOfSearch::class.java)
+        startActivity(intent)
     }
 }
